c5201d6d9ee3bb9d1df79ba68d39dd10e964fa27
Aleksei Ermakov <ermakovao89@yandex.ru>
2023-05-29 15:43:21 +0300

    adding Lesson_2/Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,18 @@
+class Person {
+    String name = "Alice";
+    int age = 25;
+    int weight = 50;
+    int hight = 165;
+    String sex = "female";
+
+    public void walk() {
+    }
+    public void sit() {
+    }
+    public void run() {
+    }
+    public void speak() {
+    }
+    public void learnJava() {
+    }
+}
