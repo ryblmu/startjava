bf30b4a588151ba88bdd78b91b9807d55c7d69c6
Aleksei Ermakov <ermakovao89@yandex.ru>
2023-05-29 10:25:57 +0300

    user -> player

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,18 +2,18 @@ class MyFirstGame {
 
     public static void main(String[] args) {
         int unknownNum = 50;
-        int userNum = 60;
+        int playerNum = 60;
 
-        while (userNum != unknownNum) {
-            if (userNum < unknownNum) {
-                System.out.println("Число " + userNum + " меньше того, что загадал компьютер");
-                userNum += 1;
+        while (playerNum != unknownNum) {
+            if (playerNum < unknownNum) {
+                System.out.println("Число " + playerNum + " меньше того, что загадал компьютер");
+                playerNum += 1;
             } else {
-                System.out.println("Число " + userNum + " больше того, что загадал компьютер");
-                userNum -= 1;
+                System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
+                playerNum -= 1;
             }
         }
 
-        System.out.println("Вы победили ! загаданное число " + userNum);
+        System.out.println("Вы победили ! загаданное число " + playerNum);
     }
 }
