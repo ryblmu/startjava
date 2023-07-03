d5e73ba4b43d5bd434053fd173aa96fa4ecdb3b0
Aleksei Ermakov <ermakovao89@yandex.ru>
2023-07-03 17:55:01 +0300

    add 3thrd qeustion

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -3,7 +3,9 @@ package com.startjava.lesson_2_3_4.array;
 public class ArrayTheme {
 
     public static void main(String[] args) {
-//        reverseArray();
+        reverseArray();
+        ratioOfArrayElements();
+        deleteElementsFromArray();
 
     }
 
@@ -16,7 +18,7 @@ public class ArrayTheme {
 
         System.out.println("Значения массива array до модификации и после:");
 
-        for(int i = array.length-1; i >= 0; i--) {
+        for (int i = array.length-1; i >= 0; i--) {
             System.out.print(array[arrayIndex] + " ");
             invertedArray[arrayIndex++] = array[i];
         }
@@ -31,7 +33,7 @@ public class ArrayTheme {
     }
 
     public static void ratioOfArrayElements() {
-        System.out.println("2. Вывод произведения элементов массива");
+        System.out.println("\n2.Вывод произведения элементов массива");
 
         int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int ratio = 1;
@@ -45,4 +47,37 @@ public class ArrayTheme {
             if (array[i] == 0 || array[i] == 9) System.out.print(i + " ");
         }
     }
+
+    public static void deleteElementsFromArray() {
+        System.out.println("\n3.Удаление элементов массива");
+
+        int arraySize = 15;
+        double[] array = new double[arraySize];
+        int midIndex = (arraySize-1) / 2;
+
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = Math.random();
+        }
+
+        double number = array[midIndex];
+        double[] arrayWithZero = array;
+
+        for (double element : array) {
+            System.out.printf("%,.3f ", element);
+        }
+
+        System.out.println();
+
+        int zeroCount = 0;
+
+        for (int i = 0; i < arraySize; i++) {
+            if (arrayWithZero[i] > number) {
+                arrayWithZero[i] = 0;
+                zeroCount++;
+            }
+            System.out.printf("%,.3f ", arrayWithZero[i]);
+        }
+        System.out.println("\nКоличество обнуленных ячеек: "+ zeroCount);
+    }
+
 }
